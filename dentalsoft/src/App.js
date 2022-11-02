c1a7ccff8b2747ea857debc0f1f7b2fe0048a37a
Alexis-Salas-Portugal <92900232+Alexis-Salas-Portugal@users.noreply.github.com>
2022-11-02 00:09:14 -0600

    Conexion con Firebase
    Se ha hecho la conexion basica al firebase

--- dentalsoft/src/App.js
+++ dentalsoft/src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  useFirebaseApp
+} from 'reactfire';
 
 function App() {
+  const firebase = useFirebaseApp();
+  console.log(firebase);
   return (
     <div className="App">
       <header className="App-header">
---
+++ dentalsoft/src/firebase-config.js
@@ -0,0 +1,10 @@
+export default 
+{
+    apiKey: "AIzaSyABql5ElgVNimXM8AgpQgMEp0ATXIi6H4k",
+    authDomain: "proyectogps-c4d09.firebaseapp.com",
+    projectId: "proyectogps-c4d09",
+    storageBucket: "proyectogps-c4d09.appspot.com",
+    messagingSenderId: "562011197376",
+    appId: "1:562011197376:web:847e2e244c41180b900225",
+    measurementId: "G-DGRE0XP84Y"
+}
